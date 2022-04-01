f2462cf4cc8d82591bce62adb30ebeb305b32b29
klayza <clayjay9870@gmail.com>
2022-04-01 14:33:53 -0700

    Foward and backward

--- Kanali/LibraryGUI.cs
+++ Kanali/LibraryGUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,28 +13,34 @@ namespace Kanali
 {
     public partial class LibraryGUI : Form
     {
+        String path = "C:/Users/cwieb255/desktop/Tree";
         Bitmap image = new Bitmap("C:/Users/cwieb255/pictures/guibg.jpg");
-        
+        String[] Images;
+        int Images_Index = 0;
+
         public LibraryGUI()
         {
             InitializeComponent();
-            pictureBoxImage.Image = image;
+            Images = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
         }
 
         private void buttonBackImage_Click(object sender, EventArgs e)
         {
-
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+            Images_Index--;
         }
 
         private void buttonForwardImage_Click(object sender, EventArgs e)
         {
-
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+            Images_Index++;
         }
 
         private void buttonFill_Click(object sender, EventArgs e)
         {
             pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBoxImage.Image = image;
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
         }
     }
 }
