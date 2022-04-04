8ab6a7dd0904b4829609677ee448fca01a3ec901
klayza <clayjay9870@gmail.com>
2022-04-04 14:33:32 -0700

    Fit feature

--- Kanali/LibraryGUI.Designer.cs
+++ Kanali/LibraryGUI.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.pictureBoxImage = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.buttonBackImage = new System.Windows.Forms.Button();
-            this.buttonForwardImage = new System.Windows.Forms.Button();
             this.buttonFill = new System.Windows.Forms.Button();
+            this.buttonForwardImage = new System.Windows.Forms.Button();
+            this.buttonBackImage = new System.Windows.Forms.Button();
+            this.labelImageIndex = new System.Windows.Forms.Label();
+            this.buttonZoomToFit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -47,6 +49,8 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.buttonZoomToFit);
+            this.panel1.Controls.Add(this.labelImageIndex);
             this.panel1.Controls.Add(this.buttonFill);
             this.panel1.Controls.Add(this.buttonForwardImage);
             this.panel1.Controls.Add(this.buttonBackImage);
@@ -55,18 +59,20 @@
             this.panel1.Size = new System.Drawing.Size(783, 33);
             this.panel1.TabIndex = 1;
             // 
-            // buttonBackImage
+            // buttonFill
             // 
-            this.buttonBackImage.Location = new System.Drawing.Point(7, 5);
-            this.buttonBackImage.Name = "buttonBackImage";
-            this.buttonBackImage.Size = new System.Drawing.Size(75, 23);
-            this.buttonBackImage.TabIndex = 0;
-            this.buttonBackImage.Text = "<--";
-            this.buttonBackImage.UseVisualStyleBackColor = true;
-            this.buttonBackImage.Click += new System.EventHandler(this.buttonBackImage_Click);
+            this.buttonFill.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFill.Location = new System.Drawing.Point(169, 5);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(75, 23);
+            this.buttonFill.TabIndex = 2;
+            this.buttonFill.Text = "Stretch";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
             // 
             // buttonForwardImage
             // 
+            this.buttonForwardImage.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonForwardImage.Location = new System.Drawing.Point(88, 5);
             this.buttonForwardImage.Name = "buttonForwardImage";
             this.buttonForwardImage.Size = new System.Drawing.Size(75, 23);
@@ -75,15 +81,35 @@
             this.buttonForwardImage.UseVisualStyleBackColor = true;
             this.buttonForwardImage.Click += new System.EventHandler(this.buttonForwardImage_Click);
             // 
-            // buttonFill
+            // buttonBackImage
             // 
-            this.buttonFill.Location = new System.Drawing.Point(169, 5);
-            this.buttonFill.Name = "buttonFill";
-            this.buttonFill.Size = new System.Drawing.Size(75, 23);
-            this.buttonFill.TabIndex = 2;
-            this.buttonFill.Text = "Fill";
-            this.buttonFill.UseVisualStyleBackColor = true;
-            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            this.buttonBackImage.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBackImage.Location = new System.Drawing.Point(7, 5);
+            this.buttonBackImage.Name = "buttonBackImage";
+            this.buttonBackImage.Size = new System.Drawing.Size(75, 23);
+            this.buttonBackImage.TabIndex = 0;
+            this.buttonBackImage.Text = "<--";
+            this.buttonBackImage.UseVisualStyleBackColor = true;
+            this.buttonBackImage.Click += new System.EventHandler(this.buttonBackImage_Click);
+            // 
+            // labelImageIndex
+            // 
+            this.labelImageIndex.AutoSize = true;
+            this.labelImageIndex.Location = new System.Drawing.Point(736, 10);
+            this.labelImageIndex.Name = "labelImageIndex";
+            this.labelImageIndex.Size = new System.Drawing.Size(0, 13);
+            this.labelImageIndex.TabIndex = 4;
+            // 
+            // buttonZoomToFit
+            // 
+            this.buttonZoomToFit.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonZoomToFit.Location = new System.Drawing.Point(250, 5);
+            this.buttonZoomToFit.Name = "buttonZoomToFit";
+            this.buttonZoomToFit.Size = new System.Drawing.Size(75, 23);
+            this.buttonZoomToFit.TabIndex = 5;
+            this.buttonZoomToFit.Text = "Fit";
+            this.buttonZoomToFit.UseVisualStyleBackColor = true;
+            this.buttonZoomToFit.Click += new System.EventHandler(this.buttonZoomToFit_Click);
             // 
             // LibraryGUI
             // 
@@ -92,10 +118,12 @@
             this.ClientSize = new System.Drawing.Size(807, 472);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBoxImage);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "LibraryGUI";
             this.Text = "LibraryGUI";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,5 +135,7 @@
         private System.Windows.Forms.Button buttonForwardImage;
         private System.Windows.Forms.Button buttonBackImage;
         private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.Label labelImageIndex;
+        private System.Windows.Forms.Button buttonZoomToFit;
     }
 }
--- Kanali/LibraryGUI.cs
+++ Kanali/LibraryGUI.cs
@@ -21,25 +21,62 @@ namespace Kanali
         public LibraryGUI()
         {
             InitializeComponent();
+
+            // Appends all files via recursion to Images array
             Images = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            // Sets first image to the picturebox
             pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+            displayImageIndex();
         }
 
         private void buttonBackImage_Click(object sender, EventArgs e)
         {
-            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+            if (Images_Index == 0)
+            {
+                Images_Index = Images.Length - 1;
+            }
+            displayImageIndex();
             Images_Index--;
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
         }
 
         private void buttonForwardImage_Click(object sender, EventArgs e)
         {
-            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+            if (Images_Index == Images.Length - 1)
+            {
+                Images_Index = 0;
+            }
             Images_Index++;
+            displayImageIndex();
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
         }
 
         private void buttonFill_Click(object sender, EventArgs e)
         {
+            if (pictureBoxImage.SizeMode == PictureBoxSizeMode.StretchImage)
+            {
+                pictureBoxImage.SizeMode = PictureBoxSizeMode.Normal;
+                return;
+            }
             pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
+        }
+
+        // Displays the current index of image ex. 1 / 5
+        private void displayImageIndex()
+        {
+            labelImageIndex.Text = (Images_Index + 1).ToString() + " / " + Images.Length.ToString();
+        }
+
+        private void buttonZoomToFit_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxImage.SizeMode == PictureBoxSizeMode.Zoom)
+            {
+                pictureBoxImage.SizeMode = PictureBoxSizeMode.Normal;
+                return;
+            }
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBoxImage.Image = new Bitmap(Images[Images_Index]);
         }
     }
