6b2c614ea7cce542e7eb7e267ff802fdb9ff1bc6
klayza <clayjay9870@gmail.com>
2022-04-07 14:29:51 -0700

    Json integration

---
+++ Kanali/Config.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Kanali
+{
+    // Contains the settings the user will define in the settings tab
+    public class Config
+    {
+        public String download_path { get; set; }
+        public Boolean media_scrolling { get; set; }
+        String filename = "Preferences.json";
+
+        // Writes over existing json, makes new instance
+        public void createJson(Config cf)
+        {
+            String jsonString = JsonSerializer.Serialize(cf);
+            File.WriteAllText(filename, jsonString);
+
+            Console.WriteLine(File.ReadAllText(filename));
+        }
+
+        // Returns the contents of Preferences.json as a type Config 
+        public Config getJson()
+        {
+            Console.WriteLine(JsonSerializer.Deserialize<Config>(File.ReadAllText(filename)).download_path);
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(filename));
+        }
+
+    }
+    // Object to hold parameters
+    public class Parameters
+    {
+        public string title;
+        public string board;
+        public List<String> whitelist;
+        public List<String> blacklist;
+
+        public Parameters(string title, string board, List<string> whitelist, List<string> blacklist)
+        {
+            this.title = title;
+            this.board = board;
+            this.whitelist = whitelist;
+            this.blacklist = blacklist;
+        }
+
+        public void addParameters(Parameters pm)
+        {
+
+        }
+
+    }
+}
--- Kanali/SettingsGUI.cs
+++ Kanali/SettingsGUI.cs
@@ -15,17 +15,32 @@ namespace Kanali
         public SettingsGUI()
         {
             InitializeComponent();
+
+            // Sets the widgets to their values retrieved from getJson() in Config class
+            Config pre = new Config();
+            Config config = pre.getJson();
+            textBoxDownloadDirectory.Text = config.download_path;
+            checkBoxMediaScrolling.Checked = config.media_scrolling;
         }
 
         private void buttonSaveAndExit_Click(object sender, EventArgs e)
         {
+            Config config = new Config();
             // Check if the user entered anything
             if (textBoxDownloadDirectory.Text == "")
             {
+                MessageBox.Show("Choose a valid path");
                 return;
             }
+
+            // Retrieves the values from widgets then creates a new Preferences.json file with those values
+            config.download_path = textBoxDownloadDirectory.Text;
+            config.media_scrolling = checkBoxMediaScrolling.Checked;
+            config.createJson(config);
+            this.Hide();
         }
 
+        // Opens prompt for user to choose a directory then adds to text box
         private void buttonChooseFolder_Click(object sender, EventArgs e)
         {    
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
